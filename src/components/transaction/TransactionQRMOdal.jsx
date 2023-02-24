0473226f469a0be049cfa2265046acded959f640
Guapura89 <betocrack1228@gmail.com>
2023-02-24 16:12:53 -0700

    feat: QR modal added :sparkles:

--- src/components/transaction/TransactionQRMOdal.jsx
+++ src/components/transaction/TransactionQRMOdal.jsx
@@ -1,6 +1,33 @@
-const TransactionQRMOdal = () => {
+import { Modal } from "../Modal";
+import { truncate } from "@/utils/String";
+import { createQR, encodeURL, findReference, validateTransfer, FindReferenceError, ValidateTransferError } from "@solana/pay";
+import { PublicKey, Keypair } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
+import { useConnection } from '@solana/wallet-adapter-react';
+import { useEffect, useRef, useState } from 'react';
+
+const TransactionQRMOdal = ({ modalOpen, setModalOpen, userAddress }) => {
+    
+    const loadQr = () => {
+
+    }
+
     return (
-        <div></div>
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+            <div >
+                <div className="flex flex-col items-center justify-center space-y-1"></div>
+
+                <div className="flex flex-col items-center justify-center space-y-1">
+                    <p className="text-lg font-medium text-gray-800">{truncate(userAddress)}</p>
+
+                    <p className="text-sm font-light text-gray-600">Scan to pay ${truncate(userAddress)}</p>
+
+                    <button onClick={() => loadQr()} className="w-full rounded-lg bg-[#16d542] py-3 hover:bg-opacity-70">
+                        <span className="font-medium text-white">Load QR code</span>
+                    </button>
+                </div>
+            </div>
+        </Modal>
     );
 }
 
