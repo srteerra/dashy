56ba72d23680befef96c686412e5ede4e47dc29b
Angel Lopez <srterradev@gmail.com>
2023-02-26 03:50:02 -0700

    merged

--- src/components/header/Profile.jsx
+++ src/components/header/Profile.jsx
@@ -14,14 +14,15 @@ const Profile = ({ setModalOpen, avatar, userAddress, userName, connected, publi
     }
 
     return (
-        <div className="flex flex-col items-center space-y-3">
-            <div onClick={onProfileOpen} className="h-20 w-20 relative cursor-pointer rounded-full border-2 border-white">
-                <img className="h-full w-full rounded-full object-cover" src={connected ? avatar : "https://certchain.infura-ipfs.io/ipfs/Qmf5tiCy77SqzAfAfXCQDuR4NPktQ8AYzkEeN9NrsnYSD6"} alt="user avatar" />
-                {connected ? <Image src={scanIcon} className="h-[30px] w-[30px] rounded-full object-cover absolute right-0 bottom-0"/> : null}
-            </div>
+        <Zoom>
+            <div className="flex flex-col items-center space-y-3">
+                <div onClick={onProfileOpen} className="h-20 w-20 relative cursor-pointer rounded-full border-2 border-white">
+                    <img className="h-full w-full rounded-full object-cover" src={connected ? avatar : "https://certchain.infura-ipfs.io/ipfs/Qmf5tiCy77SqzAfAfXCQDuR4NPktQ8AYzkEeN9NrsnYSD6"} alt="user avatar" />
+                    {connected ? <Image src={scanIcon} className="h-[30px] w-[30px] rounded-full object-cover absolute right-0 bottom-0"/> : null}
+                </div>
 
-            <div className="flex flex-col items-center space-y-1">
-                <p className="font-semibold text-white text-xl">{userName}</p>
+                <div className="flex flex-col items-center space-y-1">
+                    <p className="font-semibold text-white">{userName}</p>
 
                     <WalletMultiButton>
                         <span className='text-sm'>{ connected ? truncate(publicKey.toString()) : 'Connect Wallet'}</span>
